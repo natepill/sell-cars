8553cff5e0ee68284ae9bca7a55504842b4fe808
Kendra J. Moore <kendra.j.moore3443@gmail.com>
2018-11-03 17:41:46 -0700

    add chart

---
+++ public/js/app.js
@@ -0,0 +1,59 @@
+//Get Budget Data
+let budgetId = document.getElementById('surveyId').value;
+axios.get(`/survey/${surveyId}/api`).then((response) => {
+
+    // Data
+    var data            =   [ response.data.parkingPayment, response.data.insurancePayment, response.data.powerPayment];
+    var chart_width     =   600;
+    var chart_height    =   600;
+    var color           =   d3.scaleOrdinal( d3.schemeCategory10 );
+
+    // Pie Layout
+    var pie             =   d3.pie();
+
+    // Arc
+    var outer_radius    =   chart_width / 2;
+    var inner_radius    =   200;
+    var arc             =   d3.arc()
+        .innerRadius( inner_radius )
+        .outerRadius( outer_radius );
+
+    //Create SVG Element
+    var svg             =   d3.select("#chart")
+        .append("svg")
+        .attr("width", chart_width)
+        .attr("height", chart_height);
+
+    // Groups
+    var arcs            =   svg.selectAll( 'g.arc' )
+        .data( pie(data) )
+        .enter()
+        .append( 'g' )
+        .attr( 'class', 'arc' )
+        .attr(
+            'transform',
+            "translate(" + outer_radius + "," + chart_height / 2 + ")"
+        );
+
+    // Arcs
+    arcs.append( 'path' )
+        .attr( 'fill', function(d, i){
+            return color( i );
+        })
+        .attr( 'd', arc );
+
+    // Labels
+    arcs.append( 'text' )
+        .attr( 'transform', function(d, i){
+            return "translate(" + arc.centroid(d) + ")";
+        })
+        .attr( 'text-anchor', 'text-middle' )
+        .text(function(d){
+            return d.value;
+        });
+
+
+    console.log(response.data);
+}).catch((err) => {
+    console.log(err);
+})
